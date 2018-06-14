05ab556aa802297b1acab8c742146d201b0b7da0
Andrew Jones <andrew.f.jones@gmail.com>
2018-06-14 18:32:24 +0800

    daily commit

--- lib/src/exp.dart
+++ lib/src/exp.dart
@@ -1,5 +1,5 @@
 /*
- * power functions for longnum
+ * exponential functions for longnum
  * pow()
  * exp()
  * sqrt()
@@ -9,14 +9,28 @@
 import 'longnum/longnumclass.dart';
 import 'multiply.dart';
 
-power(longnum a, num power) {
+power(Longnum a, num power) {
 
 }
 
 power_list(List number, num exponent, [int power = 15]) {
   var number_image = number;
+  if (exponent == 0) return new Longnum();
   for (var i = 1; i < exponent; i++) {
     number = multi_int(number, number_image, power: power);
+  }
+  return number;
+}
+
+powerlist2(List number, num exponent, [int power = 15]) {
+  var i = 1;
+  var number_image = number;
+  for (i; i < exponent; i * 2) {
+    squaring(number);
+  }
+  i /= 2;
+  for (var i = 1; i < exponent; i++) {
+    number = multifull(number, number_image);
   }
   return number;
 }
--- playground.dart
+++ playground.dart
@@ -4,9 +4,21 @@ import 'lib/test.dart';
 
 void main() {
 
-  var c = new longnum.string('1234560780978798789078978907890789.987654321');
-  var b = new longnum.string('12345890809809809779887986.6545266');
+  var c = new Longnum.string('1834927612.987665432198765');
+  var b = new Longnum.string('1218646.666665');
+  var a = new Longnum.string('12.0000083648');
+  var z = new Longnum.number(12.005);
 
+  //print(long_div([48645, 19, 29445], [56], 5));
+  babylon(524288, 20);
+}
 
-  test();
+babylon(var number, var interation) {
+  //var guess = number / 2;
+  var guess = 700;
+  for (var i = 0; i < interation; i++) {
+    guess = (guess + number / guess) / 2;
+    print('guess $i: $guess');
+  }
+  return guess;
 }
